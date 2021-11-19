af9cc31502de39b8a56d65a6d17cfb4cdd2a19e3
Michael S <michaelsoumah25@gmail.com>
2021-11-19 00:00:13 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vidly-react
+# vidly-react
---
+++ src/components/common/like.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+//Input: liked: boolean
+//Output: onclick
+// site : https://fontawesome.com/v4.7/icon/heart-o
+
+const Like = props => {
+    let classes = "fa fa-heart";
+    if (!props.liked) classes += "-o";
+    return (
+        <i 
+            onClick={props.onClick}
+            style={{ cursor: "pointer" }}
+            className={classes}
+            aria-hidden="true" 
+        />
+    );
+}
+ 
+export default Like;
