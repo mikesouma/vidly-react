b73f020e9dc2c2b08619a25071e6f7d5c7d177b5
Michael S <michaelsoumah25@gmail.com>
2021-11-24 14:43:11 -0800

    feature: simple form validation .

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,24 +1,60 @@
 import React, { Component } from 'react';
-import Input from './common/'
+import Input from './common/input'
 
 class LoginForm extends React.Component {
     state = {
-        account: {username: '' , password:''}
+        account: {username: '' , password:''},
+        errors: {}
     }
 
-    handleChange = ({ currenTarget: input }) =>{
+    validate = () => {
+        const errors = {};
+        const { account } = this.state;
+        
+        if (this.state.account.username.trim() === '')
+            errors.username = 'Username is required.';
+        if (this.state.account.username.trim() === '')
+            errors.password = 'Password is required.'
+        
+        return Object.keys(errors).length === 0 ? null : errors;
+    }
+
+    validateProperty = ({ name,value }) => {
+        if (name === 'username'){
+            if (value.trim() === '') return 'Username is required.';
+            // ...
+        }
+        if (name === 'password'){
+            if (value.trim() === '') return 'Password is required.';
+            // ...
+        }
+    }
+    handleChange = ({ currentTarget: input }) => {
+        const errors = {...this.state.errors};
+        const errorMessage = this.validateProperty(input);
+
+        if (errorMessage) errors[input.name] = errorMessage;
+        else delete errors[input.name];
+
         const account = { ...this.state.account };
         account[input.name] = input.value;
-        this.setState({ account });
+
+        this.setState({ account, errors });
     };
 
     handleSubmit = e => {
         e.preventDefault();
+
+        const errors = this.validate();
+        //console.log(errors);
+        this.setState({ errors: errors || {} });
+        if (errors) return;
+
         //Call the server
         console.log("Submitted");
     }
     render() { 
-        const { account } = this.state;
+        const { account, errors } = this.state;
 
         return (
         <div>
@@ -29,12 +65,14 @@ class LoginForm extends React.Component {
                     value={account.username}
                     label="Username"
                     onChange={this.handleChange}
+                    error={errors.username}
                 />
                   <Input 
                     name="password"
                     value={account.password}
                     label="Password"
                     onChange={this.handleChange}
+                    error={errors.password}
                 />
                 <button className="btn btn-primary">Login</button>
             </form>
