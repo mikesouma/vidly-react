3478239d7c62cf50b8b0e5ba9953b6e5f99db523
Michael S <michaelsoumah25@gmail.com>
2021-11-24 12:17:51 -0800

    Refactor: Extracting a reusable Input .

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,50 +1,41 @@
 import React, { Component } from 'react';
+import Input from './common/'
 
 class LoginForm extends React.Component {
     state = {
         account: {username: '' , password:''}
     }
 
-    handleChange = e =>{
-        const account = {...this.state.account};
-        account[e.currenTarget.name] = e.currenTarget.value;
+    handleChange = ({ currenTarget: input }) =>{
+        const account = { ...this.state.account };
+        account[input.name] = input.value;
         this.setState({ account });
     };
 
     handleSubmit = e => {
         e.preventDefault();
         //Call the server
-        const username = this.username.current.value;
         console.log("Submitted");
     }
     render() { 
         const { account } = this.state;
-        
+
         return (
         <div>
             <h1>Login</h1>
             <form onSubmit={this.handleSubmit}>
-                <div className="form-froup">
-                    <label htmlFor="username">Username</label>
-                    <input 
-                        value={this.state.account.username}
-                        onChange={this.handleChange}
-                        id="username" 
-                        name="username"
-                        type="text" 
-                        className="form-control" 
-                    />
-                </div>
-                <div className="form-froup">
-                    <label htmlFor="password">Password</label>
-                    <input 
-                        value={this.state.account.password}
-                        onChange={this.handleChange}
-                        name="password"
-                        id="password" 
-                        type="text" 
-                        className="form-control" />
-                </div>
+                <Input 
+                    name="username"
+                    value={account.username}
+                    label="Username"
+                    onChange={this.handleChange}
+                />
+                  <Input 
+                    name="password"
+                    value={account.password}
+                    label="Password"
+                    onChange={this.handleChange}
+                />
                 <button className="btn btn-primary">Login</button>
             </form>
         </div>
