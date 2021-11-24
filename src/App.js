07691a0cbe6c9d8db3c65e2c965f1329b5275c1f
Michael S <michaelsoumah25@gmail.com>
2021-11-23 18:15:28 -0800

    feature: adding Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import logo from './logo.svg';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import './App.css';
 
 class App extends React.Component {
   render() { 
     return(
       <main className="container">
-        <Movies />
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
     );
   }
